3713556c3ab820d349de01707c9d272d1491a917
Sherlock1956 <1346787051@qq.com>
2025-09-04 12:13:34 +0800

    finish tokenize_prompt_and_output

--- cs336_alignment/download_models.py
+++ cs336_alignment/download_models.py
@@ -2,10 +2,10 @@ from modelscope import snapshot_download
 
 def download_qwen25_math():
     # 模型仓库名称（在 ModelScope 上的 ID）
-    repo_id = "qwen/Qwen2.5-Math-7B"
+    repo_id = "qwen/Qwen2.5-Math-1.5B"
 
     # 本地保存目录
-    local_dir = "/root/autodl-tmp/lyx/assignment5-alignment/models/Qwen2.5-Math-7B"
+    local_dir = "/Users/lyx/Downloads/Study/projects/python/CS336-assignment5/models/Qwen2.5-Math-1.5B"
 
     print(f"Downloading {repo_id} from ModelScope to {local_dir} ...")
 
@@ -16,7 +16,7 @@ def download_qwen25_math():
         revision="master"  # 可以指定分支/版本
     )
 
-    print("✅ Download completed!")
+    print("Download completed!")
 
 if __name__ == "__main__":
     download_qwen25_math()
--- cs336_alignment/utils.py
+++ cs336_alignment/utils.py
@@ -16,17 +16,46 @@ def tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer):
     input_ids: torch.Tensor of shape (batch_size, max(prompt_and_output_lens) - 1): the tokenized prompt and output strings, with the final token sliced off.
     labels: torch.Tensor of shape (batch_size, max(prompt_and_output_lens) - 1): shifted input ids, i.e., the input ids without the first token. 
     response_mask: torch.Tensor of shape (batch_size, max(prompt_and_output_lens) - 1): a mask on the response tokens in the labels."""
-    prompt_len = len(prompt_strs)
-    output_len = len(output_strs)
-    input_ids = tokenizer.tokenize(prompt_strs)
-    output_ids = tokenizer.tokenize(output_strs)
-    input_ids_full = torch.concat((input_ids, output_ids),dim=-1)
-    input_ids = input_ids_full[:-1]
-    labels = input_ids_full[1:]
-    response_mask = torch.zeros_like(labels)
-    response_mask[:prompt_len - 1] = 1.0
+    prompt_and_output_lens = []
+    response_mask = []
+    input_ids = []
+    labels = []
+    for i in range(len(prompt_strs)):
+        input_id = tokenizer.encode(prompt_strs[i])
+        output_id = tokenizer.encode(output_strs[i])
+        input_id_full = input_id + output_id
+        local_len = len(input_id) + len(output_id)
+        prompt_and_output_lens.append(local_len)
+        mask = [0.0] * (local_len - 1)
+        mask[len(input_id) - 1:] = [1.0] * len(output_id)
+        response_mask.append(mask)
+        input_ids.append(input_id_full[:-1])
+        labels.append(input_id_full[1:])
+    max_len = max(prompt_and_output_lens)
+    for i in range(len(prompt_strs)):
+        if prompt_and_output_lens[i] < max_len:
+            padding_num = max_len - prompt_and_output_lens[i]
+            input_ids[i] = input_ids[i] + [tokenizer.pad_token_id] * padding_num
+            labels[i] = labels[i] + [tokenizer.pad_token_id] * padding_num
+            response_mask[i] = response_mask[i] + [0.0] * padding_num
+    input_ids = torch.tensor(input_ids)
+    labels = torch.tensor(labels)
+    response_mask = torch.tensor(response_mask)
     return {
         "input_ids": input_ids,
         "labels": labels,
         "response_mask": response_mask
     }
+if __name__ == "__main__":
+    tokenizer = AutoTokenizer.from_pretrained("/Users/lyx/Downloads/Study/projects/python/CS336-assignment5/models/Qwen2.5-Math-1.5B/qwen/Qwen2___5-Math-1___5B")
+    prompt_strs = [
+        "Hello, world!",
+        "This is a test.",
+        "This is another test.",
+    ]
+    output_strs = [
+        "Hello, world!",
+        "This is a test.",
+        "This is another test.",
+    ]
+    tokenize_prompt_and_output(prompt_strs, output_strs, tokenizer)
