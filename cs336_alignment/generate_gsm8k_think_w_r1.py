de5a3b19893a08b3722cf0d6507de6fd431353ad
Sherlock <1346787051@qq.com>
2025-09-15 00:31:05 +0800

    update note document

--- cs336_alignment/generate_gsm8k_think_w_r1.py
+++ cs336_alignment/generate_gsm8k_think_w_r1.py
@@ -2,7 +2,7 @@
 
 from openai import OpenAI
 
-client = OpenAI(api_key="sk-fc6b700283ee443f9df92cf8d817a147", base_url="https://api.deepseek.com")
+client = OpenAI(api_key="***", base_url="https://api.deepseek.com")
 prompt = """A conversation between User and Assistant. The User asks a question, and the Assistant solves it. The Assistant first thinks about the reasoning process in the mind and then provides the User with the answer. The reasoning process is enclosed within <think> </think> and answer is enclosed within <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>.
 User: {question}
 Assistant: <think>"""
